2fb7e9b051b70d6eaa7a18723d898292c000bcd0
nickolas360 <nickolas360@users.noreply.github.com>
2015-06-05 17:41:32 -0700

    Initial code commit

---
+++ nimbot.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+# Copyright (C) 2015 nickolas360 (https://github.com/nickolas360)
+#
+# This file is part of nimbot.
+#
+# nimbot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# nimbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with nimbot.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Usage:
+  nimbot <host> <port> <channel> [-i] [-n nick]
+
+Options:
+  -i --identify  Identify with NickServ. Accepts a password through stdin.
+  -n nick        The nickname to use [default: nimbot].
+"""
+from pyrcb import IrcBot
+from mention import Mention
+from docopt import docopt
+from collections import defaultdict
+from datetime import datetime, timedelta
+from humanize import naturaltime
+from threading import Event
+import os
+import re
+import sys
+
+# If modified, replace the source URL with one to the modified version.
+help_message = """\
+nimbot: The non-intrusive mailbot.
+Source: https://github.com/nickolas360/nimbot (AGPLv3 or later)
+nimbot is {0}.
+Usage:
+  help     Show this help message.
+  check    Manually check for mail.
+  clear    Clear all mail without reading.
+  enable   Enable nimbot.
+  disable  Disable nimbot.
+"""
+
+
+class Nimbot(IrcBot):
+    def __init__(self):
+        super(Nimbot, self).__init__()
+        self.msg_index = -1
+        self.names = []
+
+        self.mentions = defaultdict(list)
+        self.enabled = defaultdict(lambda: True)
+        self.save_event = Event()
+
+        self.script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        self.prefs_path = os.path.join(self.script_dir, "prefs")
+        self.read_prefs()
+
+    def deliver(self, nickname, mentions):
+        for mention in mentions:
+            message = "[{0}] <{1}> {2}".format(
+                naturaltime(mention.time), mention.sender, mention.message)
+            self.send(nickname, message)
+            print("[deliver -> {0}] {1}".format(nickname, message))
+
+    def on_query(self, message, nickname):
+        cmd = message.lower()
+        nick = nickname.lower()
+        print("[query] <{0}> {1}".format(nickname, message))
+
+        if cmd == "help":
+            help_lines = help_message.format(
+                ["disabled", "enabled"][self.enabled[nick]]).splitlines()
+            for line in help_lines:
+                self.send(nick, line)
+        elif cmd == "check":
+            if not self.enabled[nick]:
+                self.send(nick, "nimbot is disabled.")
+            elif self.mentions[nick]:
+                self.deliver(nick, self.mentions[nick])
+                self.mentions[nick] = []
+            else:
+                self.send(nick, "No new mail.")
+        elif cmd == "clear":
+            self.mentions[nick] = []
+            self.send(nick, "Mail cleared.")
+        elif cmd == "enable" or cmd == "disable":
+            if self.enabled[nick] != (cmd == "enable"):
+                self.enabled[nick] = (cmd == "enable")
+                self.mentions[nick] = []
+                self.save_event.set()
+            self.send(nick, "nimbot {0}d.".format(cmd))
+        else:
+            self.send(nick, '"/msg {0} help" for help.'.format(self.nickname))
+
+    def on_message(self, message, nickname, target, is_query):
+        if is_query:
+            self.on_query(message, nickname)
+            return
+
+        self.msg_index += 1
+        nick = nickname.lower()
+        mentioned = defaultdict(bool)
+        print("[{0}] <{1}> {2}".format(target, nickname, message))
+
+        for name in self.names:
+            if re.search(r"\b{0}\b".format(name), message):
+                self.mentions[name].append(Mention(
+                    message, nick, name, self.msg_index, datetime.now()))
+                mentioned[name] = True
+
+        if mentioned:
+            print("[mentioned] {0}".format(", ".join(mentioned)))
+        if not self.enabled[nick]:
+            return
+
+        for mention in self.mentions[nick]:
+            deliver = self.msg_index - mention.index > 50 and (
+                      not mentioned[mention.sender] or
+                      any(self.msg_index - m.index < 40
+                          for m in self.mentions[nick]
+                          if m.sender == mention.sender))
+            if deliver:
+                self.deliver(nick, [mention])
+        self.mentions[nick] = []
+
+    def on_join(self, nickname, channel):
+        nick = nickname.lower()
+        if nick == self.nickname.lower():
+            return
+        if nick not in self.names:
+            self.names.append(nick)
+            self.save_event.set()
+            return
+        if self.enabled[nick]:
+            self.deliver(nick, self.mentions[nick])
+            self.mentions[nick] = []
+
+    def on_nick(self, nickname, new_nickname, is_self):
+        if is_self:
+            return
+        nick = nickname.lower()
+        new_nick = new_nickname.lower()
+        if new_nick not in self.names:
+            self.names.append(new_nick)
+            self.enabled[new_nick] = self.enabled[nick]
+            self.save_event.set()
+
+    def on_names(self, channel, names):
+        for name in [n.lower() for n in names]:
+            if name not in self.names and name != self.nickname.lower():
+                self.names.append(name)
+                self.save_event.set()
+
+    def read_prefs(self):
+        if not os.path.isfile(self.prefs_path):
+            open(self.prefs_path, "w").close()
+            return
+
+        with open(self.prefs_path) as f:
+            for line in f:
+                if not line.startswith("#"):
+                    name = line.split()[0]
+                    self.names.append(name)
+                    self.enabled[name] = line.split()[1] == "True"
+
+    def save_prefs(self):
+        with open(self.prefs_path, "w") as f:
+            print("# [name (lowercase)] [enabled (True/False)]", file=f)
+            for name in self.names:
+                print(name, self.enabled[name], file=f)
+
+    def save_loop(self):
+        while self.is_alive():
+            self.save_event.clear()
+            self.save_event.wait()
+            self.save_prefs()
+
+
+def main():
+    args = docopt(__doc__)
+    bot = Nimbot()
+    bot.connect(args["<host>"], int(args["<port>"]))
+
+    if args["--identify"]:
+        print("Password: ", end="", file=sys.stderr)
+        bot.password(input())
+    bot.register(args["-n"])
+
+    bot.join(args["<channel>"])
+    bot.listen_async(bot.save_event.set)
+    bot.save_loop()
+    print("Disconnected from server.")
+
+if __name__ == "__main__":
+    main()
---
+++ pyrcb.py
@@ -0,0 +1,233 @@
+# Copyright (C) 2015 nickolas360 (https://github.com/nickolas360)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+from multiprocessing.pool import ThreadPool
+import re
+import socket
+import ssl
+import threading
+
+__version__ = "1.4.1"
+
+
+class IrcBot(object):
+    def __init__(self, debug_print=False, print_function=print):
+        self.debug_print = debug_print
+        self.print_function = print_function
+        self.thread_pool = ThreadPool(processes=16)
+
+        self._buffer = ""
+        self.socket = socket.socket()
+        self.hostname = None
+        self.port = None
+
+        self._names = []
+        self.channels = []
+        self.alive = False
+        self.is_registered = False
+        self.nickname = None
+
+    def connect(self, hostname, port, use_ssl=False, ca_certs=None):
+        self._cleanup()
+        self.hostname = hostname
+        self.port = port
+        if use_ssl:
+            reqs = ssl.CERT_REQUIRED if ca_certs else ssl.CERT_NONE
+            self.socket = ssl.wrap_socket(
+                self.socket, cert_reqs=reqs, ca_certs=ca_certs)
+        self.socket.connect((hostname, port))
+        self.alive = True
+
+    def password(self, password):
+        self._writeline("PASS :{0}".format(password))
+
+    def register(self, nickname):
+        self.nickname = nickname
+        self._writeline("USER {0} 8 * :{0}".format(nickname))
+        self._writeline("NICK {0}".format(nickname))
+        while not self.is_registered:
+            line = self._readline()
+            if line is None:
+                return
+            if self._parse(line)[1] == "433":  # ERR_NICKNAMEINUSE
+                raise ValueError("Nickname is already in use")
+            self._handle(line)
+
+    def join(self, channel):
+        self._writeline("JOIN {0}".format(channel))
+
+    def part(self, channel, message=None):
+        if channel.lower() in self.channels:
+            part_msg = " :" + message if message else ""
+            self._writeline("PART {0}{1}".format(channel, part_msg))
+            self.channels.remove(channel.lower())
+
+    def quit(self, message=None):
+        try:
+            quit_msg = " :" + message if message else ""
+            self._writeline("QUIT{0}".format(quit_msg))
+            self.socket.shutdown(socket.SHUT_RDWR)
+        except socket.error:
+            pass
+        self.socket.close()
+        self.alive = False
+
+    def nick(self, new_nickname):
+        self._writeline("NICK {0}".format(new_nickname))
+
+    def names(self, channel):
+        if not channel.isspace():
+            self._writeline("NAMES {0}".format(channel))
+
+    def send(self, target, message):
+        self._writeline("PRIVMSG {0} :{1}".format(target, message))
+
+    def send_notice(self, target, message):
+        self._writeline("NOTICE {0} :{1}".format(target, message))
+
+    def send_raw(self, message):
+        self._writeline(message)
+
+    def on_join(self, nickname, channel):
+        pass
+
+    def on_part(self, nickname, channel, message):
+        pass
+
+    def on_quit(self, nickname, message):
+        pass
+
+    def on_kick(self, nickname, channel, target, is_self):
+        pass
+
+    def on_nick(self, nickname, new_nickname, is_self):
+        pass
+
+    def on_names(self, channel, names):
+        pass
+
+    def on_message(self, message, nickname, target, is_query):
+        pass
+
+    def on_notice(self, message, nickname, target, is_query):
+        pass
+
+    def on_other(self, nickname, command, args):
+        pass
+
+    def listen(self, async_events=False):
+        while True:
+            try:
+                line = self._readline()
+            except socket.error:
+                self.quit()
+                return
+            if line is None:
+                return
+            else:
+                self._handle(line)
+
+    def listen_async(self, callback=None, async_events=False):
+        def target():
+            self.listen(async_events)
+            if callback:
+                callback()
+        t = threading.Thread(target=target)
+        t.daemon = True
+        t.start()
+
+    def is_alive(self):
+        return self.alive
+
+    def _handle(self, message, async_events=False):
+        def async(target, *args):
+            if async_events:
+                self.thread_pool.apply_async(target, args)
+            else:
+                target(*args)
+
+        nick, cmd, args = self._parse(message)
+        is_self = (nick or "").lower() == self.nickname.lower()
+
+        if cmd == "PING":
+            self._writeline("PONG :{0}".format(args[0]))
+        elif cmd == "MODE":
+            self.is_registered = True
+        elif cmd == "JOIN":
+            if is_self:
+                self.channels.append(args[0])
+            async(self.on_join, nick, args[0])
+        elif cmd == "PART":
+            async(self.on_part, nick, args[0], args[1])
+        elif cmd == "QUIT":
+            async(self.on_quit, nick, args[0])
+        elif cmd == "KICK":
+            is_self = args[1].lower() == self.nickname.lower()
+            async(self.on_kick, nick, args[0], args[1], is_self)
+        elif cmd == "NICK":
+            if is_self:
+                self.nickname = args[0]
+            async(self.on_nick, nick, args[0], is_self)
+        elif cmd == "353":  # RPL_NAMREPLY
+            names = args[-1].replace("@", "").replace("+", "").split()
+            self._names.append((args[-2], names))
+        elif cmd == "366":  # RPL_ENDOFNAMES
+            for channel, names in self._names:
+                async(self.on_names, channel, names)
+            if not self._names:
+                async(self.on_names, None, None)
+            self._names = []
+        elif cmd == "PRIVMSG" or cmd == "NOTICE":
+            is_query = args[0].lower() == self.nickname.lower()
+            target = nick if is_query else args[0]
+            event = self.on_message if cmd == "PRIVMSG" else self.on_notice
+            async(event, args[-1], nick, target, is_query)
+        else:
+            async(self.on_other, nick, cmd, args)
+
+    def _parse(self, message):
+        match = re.match(r"(?::([^!@ ]+)[^ ]* )?([^ ]+)"
+                         r"((?: [^: ][^ ]*){0,14})(?: :?(.+))?",
+                         message)
+
+        nick, cmd, args, trailing = match.groups()
+        args = (args or "").split()
+        if trailing:
+            args.append(trailing)
+        return (nick, cmd, args)
+
+    def _readline(self):
+        while "\r\n" not in self._buffer:
+            data = self.socket.recv(1024)
+            if len(data) == 0:
+                self._cleanup()
+                return
+            self._buffer += data.decode("utf8", "ignore")
+
+        line, self._buffer = self._buffer.split("\r\n", 1)
+        if self.debug_print:
+            self.print_function(line)
+        return line
+
+    def _writeline(self, data):
+        self.socket.sendall((data + "\r\n").encode("utf8", "ignore"))
+        if self.debug_print:
+            self.print_function(">>> " + data)
+
+    def _cleanup(self):
+        self._buffer = ""
+        self.is_registered = False
+        self.channels = []
