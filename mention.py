923feab5a0f37dfb2c3ab78ce1b2c8a5eccf08a1
nickolas360 <nickolas360@users.noreply.github.com>
2015-07-19 22:07:06 -0700

    Save mentions on exit, add interactive commands

--- mention.py
+++ mention.py
@@ -15,11 +15,28 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with nimbot.  If not, see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime
+
 
 class Mention:
-    def __init__(self, message, sender, index, time, private=False):
+    def __init__(self, message, sender, target, index, time, private=False):
         self.message = message
         self.sender = sender
+        self.target = target
         self.index = index
         self.time = time
         self.private = private
+
+    def to_string(self, offset=0):
+        return "{0} {1} {2} {3} {4} {5}".format(
+              self.time.strftime("%Y-%m-%d/%H:%M:%S"), self.index - offset,
+              self.private, self.sender, self.target, self.message)
+
+    @classmethod
+    def from_string(cls, string):
+        split = string.rstrip().split(" ", 5)
+        time = datetime.strptime(split[0], "%Y-%m-%d/%H:%M:%S")
+        index = int(split[1])
+        private = split[2] == "True"
+        sender, target, message = split[3:]
+        return cls(message, sender, target, index, time, private)
